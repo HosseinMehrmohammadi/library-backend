160fc5cf2028488e90ef11b2b64c6c8f5dfb82e9
hossein.mehrm <hossein.m.1459@gmail.com>
2023-07-18 15:00:08 +0330

    Feat: v1.0.4 - add books path and view

--- library/books/views.py
+++ library/books/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Book
 
 def books(request):
-    return HttpResponse()
+    books = Book.objects.all().values()
+    template = loader.get_template('books.html')
+    context = {
+        'books': books,
+    }
+    return HttpResponse(template.render(context, request))
