b26e14804ad290e1ba8be4c8c55e85ab87ad401a
hossein.mehrm <hossein.m.1459@gmail.com>
2023-07-18 13:34:04 +0330

    Feat: v1.0.2 - add book model

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,29 @@
+import uuid
+import datetime
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    id = models.UUIDField(
+        primary_key = True,
+        default = uuid.uuid4,
+        editable = False
+    )
+    title = models.CharField(
+        max_length = 255
+    )
+    author = models.CharField(
+        max_length = 255
+    )
+    genre = models.CharField(
+        max_length = 255,
+        null = True
+    )
+    published_year = models.IntegerField(
+        null = True   
+    )
+    is_available = models.BooleanField(
+        default = False
+    )
+    created_at = models.DateField(
+        default = datetime.datetime.now()
+    )
