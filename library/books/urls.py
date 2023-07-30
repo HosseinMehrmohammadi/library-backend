fc18b41886a0c1b97bde0d70a21c688c587ba4c9
hossein.mehrm <hossein.m.1459@gmail.com>
2023-07-30 15:57:07 +0330

    Refactor: v1.3.0 - base book views on rest frame work

--- library/books/urls.py
+++ library/books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('', views.books, name='books'),
+    path('', csrf_exempt(views.books), name='books'),
     path('<uuid:id>', views.get_book, name='get_book'),
-    path('add', views.add_book, name='add_book'),
+    path('add', csrf_exempt(views.add_book), name='add_book'),
 ]
--- library/books/views.py
+++ library/books/views.py
@@ -1,44 +1,101 @@
 import json
-from django.http import HttpResponse
-from django.template import loader
+from rest_framework import status
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 from .models import Book
 
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
 def books(request):
-    books = Book.objects.all().values()
-    template = loader.get_template('books.html')
-    context = {
-        'books': books,
-    }
+    try: 
+        books = Book.objects.all().values()
+        books = list(book for book in books)
+        json_response = {
+            'response_id': '',
+            'response_message': 'Get Books Success.',
+            'data': str(books),
+        }
 
-    return HttpResponse(template.render(context, request))
-
-def get_book(request, id):
-    book = Book.objects.get(id = id)
-    template = loader.get_template('book.html')
-    context = {
-        'book': book,
-    }
-
-    return HttpResponse(template.render(context, request))
-
-def add_book(request):
-    book = json.loads(request.body)
-    new_book = Book(
-        title = book['title'],
-        author = book['author'],
-        genre = 
-            book['genre'] 
-            if 'genre' in book 
-            else None,
-        published_year = 
-            book['published_year'] 
-            if 'published_year' in book 
-            else None,
-        is_available = 
-            book['is_available'] 
-            if 'is_available' in book 
-            else False,
-    )
-    new_book.save()
+        return Response(json_response, status=status.HTTP_200_OK)
     
-    return HttpResponse('add book success')
+    except Exception as e:
+        json_response = {
+            'response_id': '',
+            'response_message': 'Get Books Failed.',
+            'error': str(e)
+        }
+        return Response(json_response, status=status.HTTP_417_EXPECTATION_FAILED)
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def get_book(request, id):
+    try: 
+        book = Book.objects.get(id = id)
+        json_response = {
+            'response_id': '',
+            'response_message': 'Get Book Success.',
+            'data': str(book),
+        }
+
+        return Response(json_response, status=status.HTTP_200_OK)
+    
+    except Exception as e:
+        json_response = {
+            'response_id': '',
+            'response_message': 'Get Book Failed.',
+            'error': str(e),
+        }
+
+        return Response(json_response, status=status.HTTP_417_EXPECTATION_FAILED)
+    
+@api_view(['POST'])
+@renderer_classes([JSONRenderer])
+def add_book(request):
+    try:
+        book = json.loads(request.body)
+        new_book = Book(
+            title = book['title'],
+            author = book['author'],
+            genre = 
+                book['genre'] 
+                if 'genre' in book 
+                else None,
+            published_year = 
+                book['published_year'] 
+                if 'published_year' in book 
+                else None,
+            is_available = 
+                book['is_available'] 
+                if 'is_available' in book 
+                else False,
+        )
+        new_book.save()
+
+        json_response = {
+            'response_id': '',
+            'response_message': 'Add Book Success.',
+        }
+
+        return Response(json_response, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        json_response = {
+            'response_id': '',
+        }
+
+        if type(e) == KeyError:
+            json_response.update({
+                'response_message': 'Error in Request Model.',
+                'error': str(e),
+            })
+
+            return Response(json_response, status=status.HTTP_400_BAD_REQUEST)
+        
+        else:
+            json_response.update({
+                'response_message': 'Add Book Failed.',
+                'error': str(e),
+            })
+        
+            return Response(json_response, status=status.HTTP_417_EXPECTATION_FAILED)
